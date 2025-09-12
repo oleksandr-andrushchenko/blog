829f429e4a6512064ff3e87634c676b3725c7d0c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-11 23:46:17 -0600

    user page: basic ui/ux implemented, avatar added; soft non-func refactoring

--- be-function-src/utils.py
+++ be-function-src/utils.py
@@ -219,6 +219,12 @@ def get_live_config(load_env=False):
                 Permission.ALL
             ],
         },
+        "default_avatar_colors": [
+            "#F44336", "#E91E63", "#9C27B0", "#673AB7",
+            "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4",
+            "#009688", "#4CAF50", "#8BC34A", "#CDDC39",
+            "#FFEB3B", "#FFC107", "#FF9800", "#FF5722"
+        ],
         **json.load(open("./data.json"))
     }
 
