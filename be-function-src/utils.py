caf9d308f4f9d6624897ee47a776bb0323ec8555
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-10-03 03:21:09 -0600

    dummy data updated

--- be-function-src/utils.py
+++ be-function-src/utils.py
@@ -937,6 +937,17 @@ async def create_dummy_fixtures() -> None:
         return
     user_token = get_dummy_user_token()
     user = await upsert_user_by_user_token(user_token)
+    user.name = "John Doe"
+    user.username = "j_doe"
+    user.headline = "Software Engineer"
+    user.website = "https://example.com"
+    user.about = ("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
+                  "industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type "
+                  "and scrambled it to make a type specimen book. It has survived not only five centuries, but also the "
+                  "leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s "
+                  "with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
+                  "publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+    user.address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
     posts = [
         PostDTO(
             title="Post title #111111111111111111111111",
