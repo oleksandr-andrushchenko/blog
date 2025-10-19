f5c69b53520efee553c95b9cd153b8e0eb5d1efa
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-10-19 04:42:12 -0600

    soft non-func refactoring

--- scripts/extract_dynamodb_schema.py
+++ scripts/extract_dynamodb_schema.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python3
-import yaml
+
 import json
+from utils import get_dynamodb_schema
 
-CF_FILE = "cf.yml"
-RESOURCE_NAME = "DynamoDBTable"
-
-
-# Ignore unknown CF tags like !Sub, !Ref
-def ignore_unknown(loader, tag_suffix, node):
-    return loader.construct_scalar(node)
-
-
-yaml.add_multi_constructor("!", ignore_unknown)
-
-with open(CF_FILE, "r") as f:
-    cf = yaml.load(f, Loader=yaml.FullLoader)
-
-props = cf.get("Resources", {}).get(RESOURCE_NAME, {}).get("Properties", {})
-
-# Keep only DynamoDB keys
-allowed_keys = {"TableName", "BillingMode", "AttributeDefinitions", "KeySchema", "GlobalSecondaryIndexes"}
-schema = {k: v for k, v in props.items() if k in allowed_keys}
-
-print(json.dumps(schema, indent=2))
+print(json.dumps(get_dynamodb_schema(), indent=2))
