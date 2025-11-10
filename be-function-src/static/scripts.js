7afc6377fced28eea9c6e18660cddd052271445b
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-09 21:17:41 -0700

    soft non-func refactoring

--- be-function-src/static/scripts.js
+++ be-function-src/static/scripts.js
@@ -575,7 +575,8 @@ if (window.CONFIG.init_tinymce) {
     skin: "bootstrap",
     plugins: "importcss insertdatetime preview autolink code fullscreen image link codesample table charmap advlist lists autosave",
     menubar: false,
-    toolbar: "undo redo | h2 h3 blockquote | bold italic underline strikethrough | align numlist bullist | outdent indent | " + "link image table charmap codesample insertdatetime | code fullscreen preview",
+    toolbar: "undo redo | h2 h3 blockquote | bold italic underline strikethrough | align numlist bullist | outdent indent | "
+      + "link image table charmap codesample insertdatetime | code fullscreen preview",
     autosave_ask_before_unload: true,
     contextmenu: false,
     content_css: ["default", ...window.CONFIG.css_filenames],
@@ -586,7 +587,13 @@ if (window.CONFIG.init_tinymce) {
     powerpaste_allow_local_images: true,
     powerpaste_word_import: "clean",
     powerpaste_html_import: "clean",
-    valid_elements: "figure[class],figcaption[class]," + "img[src|alt|title|class|width|height|style]," + "h2,h3,h4,h5,h6," + "a[href|target|rel|title]," + "b/strong,i/em,u,span[class]," + "ul,ol,li," + "table[class|border|cellpadding|cellspacing],thead,tbody,tfoot,tr,th[colspan|rowspan],td[colspan|rowspan]," + "div[class],br,p,pre[class],code[class],blockquote",
+    valid_elements: "figure[class],figcaption[class]," + "img[src|alt|title|class|width|height|style],"
+      + "h2,h3,h4,h5,h6,"
+      + "a[href|target|rel|title],"
+      + "b/strong,i/em,u,span[class],"
+      + "ul,ol,li,"
+      + "table[class|border|cellpadding|cellspacing],thead,tbody,tfoot,tr,th[colspan|rowspan],td[colspan|rowspan],"
+      + "div[class],br,p,pre[class],code[class],blockquote",
     document_base_url: window.CONFIG.base_url + "/",
     table_default_attributes: {class: "table"},
     table_class_list: [{title: "Regular", value: "table"}, {
