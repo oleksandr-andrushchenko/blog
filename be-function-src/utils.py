0fce9b48298a7c3ef7a928d43169f47c1b091822
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-12-21 20:08:07 -0700

    post title & content length rules updated

--- be-function-src/utils.py
+++ be-function-src/utils.py
@@ -259,8 +259,8 @@ def sanitize_tags(value):
 
 
 class PostDTO(BaseModel):
-    title: str = Field(..., min_length=20, max_length=500)
-    content: str = Field(..., min_length=1000, max_length=10000)
+    title: str = Field(..., min_length=10, max_length=500)
+    content: str = Field(..., min_length=1000, max_length=12000)
     tags: conlist(constr(min_length=2, max_length=40), min_length=1, max_length=3)
 
     @field_validator("tags", mode="before")
